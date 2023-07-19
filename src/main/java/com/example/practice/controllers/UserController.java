a646fc9ce1ef2e4038d4ae9337c2788ee151ca31
Shree Kishna Thapa Magar <SHREEKRISHNA@lancemeup.com>
2023-07-19 17:54:24 +0545

    remove logout and added dependencies postgresql

--- src/main/java/com/example/practice/controllers/UserController.java
+++ src/main/java/com/example/practice/controllers/UserController.java
@@ -4,7 +4,6 @@ import com.example.practice.entities.ChangePasswordDto;
 import com.example.practice.entities.LoginUserDto;
 import com.example.practice.entities.RegisterUserDto;
 import com.example.practice.services.CustomUserDetailService;
-import com.example.practice.services.LogoutService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -18,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class UserController {
     private CustomUserDetailService customUserDetailService;
-    private LogoutService logoutService;
-
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody @Validated RegisterUserDto registerRequest) {
@@ -35,7 +32,4 @@ public class UserController {
     public ResponseEntity<?>changePassword(@RequestBody @Validated ChangePasswordDto passwordRequest){
         return customUserDetailService.changePassword(passwordRequest);
     }
-
-
-
 }
